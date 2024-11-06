7b623ea3a3115ea81b7517d80cabf8d2d673b81e
Nguyễn Thị Mỹ Duyên <mduyen304@gmail.com>
2024-11-06 15:58:21 +0700

    Lab3

--- Lab3-Ex1.cpp
+++ Lab3-Ex1.cpp
@@ -12,7 +12,7 @@ int binarySearchLeft(int a[], int n, int target) {
     int left = 0, right = n - 1;
     while (left <= right) {
         int mid = left + (right - left) / 2;
-        if (a[mid] < target) {
+        if (a[mid] <= target) {
             left = mid + 1;
         } else {
             right = mid - 1;
